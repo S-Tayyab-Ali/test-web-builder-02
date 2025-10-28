c9f978f84e09d431b8eef4e018d716edf8fb3e1d
Freestyle Dev Server <dev-server@freestyle.sh>
2025-10-28 00:52:06 +0000

    feat: overhaul css and home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
-"use client"
-import { useState } from "react";
-
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>Hello World</div>
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <div className="text-center">
+        <h1 className="text-6xl font-bold">Star Wars</h1>
+        <p className="mt-4 text-2xl">A long time ago in a galaxy far, far away....</p>
+      </div>
     </main>
   );
 }
+
