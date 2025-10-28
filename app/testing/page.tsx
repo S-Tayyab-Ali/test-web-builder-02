b3ef3af35d3becbef7f9915d6067ead7a6bd3cf7
Freestyle Dev Server <dev-server@freestyle.sh>
2025-10-28 00:49:23 +0000

    feat: add button to testing page

--- app/testing/page.tsx
+++ app/testing/page.tsx
@@ -1,9 +1,13 @@
 "use client"
 
+import { Button } from "@/components/ui/button";
+
 export default function Testing() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>This is the testing page</div>
+      <Button>Click me</Button>
     </main>
   );
 }
+
